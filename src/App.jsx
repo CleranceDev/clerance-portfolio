d4cd486874f009f52fabed1659647c06c399df12
CleranceDev <your-email@associated-with-clerancedev-account.com>
2025-06-08 03:27:17 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Home from "./Pages/Home";
-
+import {BrowserRouter as Router ,Routes, Route} from 'react-router-dom'
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <>
+    <Router basename="/clerance-portfolio">
       <Navbar />
-
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 };
 
